0595265653355b0fa877d6a682a8791ac0f196b5
Golla Pujari Sowmya <121338921+sowmyayadav12@users.noreply.github.com>
2023-11-28 01:22:41 +0530

    Create group-icon.js

---
+++ components/group-icon.js
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+const GroupIcon = memo(() => {
+  return (
+    <img
+      className="absolute top-[0px] left-[0px] w-[1440px] h-[171px]"
+      alt=""
+      src="/group-2.svg"
+    />
+  );
+});
+
+export default GroupIcon;
