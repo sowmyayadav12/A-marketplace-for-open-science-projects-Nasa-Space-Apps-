8ea8940b42340b3c60be01d19395672c688c7070
Golla Pujari Sowmya <121338921+sowmyayadav12@users.noreply.github.com>
2023-11-28 01:15:56 +0530

    Create cost-link.js

---
+++ components/cost-link.js
@@ -0,0 +1,34 @@
+import { memo, useMemo } from "react";
+import Link from "next/link";
+
+const CostLink = memo(({ cOSTLeft, cOSTTop }) => {
+  const groupAStyle = useMemo(() => {
+    return {
+      left: cOSTLeft,
+      top: cOSTTop,
+    };
+  }, [cOSTLeft, cOSTTop]);
+
+  return (
+    <Link
+      className="cursor-pointer [text-decoration:none] absolute top-[327px] left-[203px] w-80 h-[420px] text-left text-3xl text-white font-poppins"
+      href="/p-r-o-j-e-c-t"
+      style={groupAStyle}
+    >
+      <a className="[text-decoration:none] absolute top-[7.27px] left-[0px] rounded-6xl bg-darkslategray-200 w-[316.07px] h-[412.73px]" />
+      <b className="absolute top-[366.23px] left-[15.71px] inline-block w-[223.8px] h-[30.52px] text-tomato">
+        <span>COST</span>
+        <span className="text-white">:XYZ</span>
+      </b>
+      <div className="absolute top-[0px] left-[21.59px] rounded-6xl bg-gainsboro-200 w-[288.59px] h-[217.99px]" />
+      <b className="absolute top-[258.69px] left-[15.71px] text-xl inline-block w-[304.29px] h-[82.84px]">
+        Small description in less than 50 words
+      </b>
+      <b className="absolute top-[225.26px] left-[54.97px] inline-block w-[227.73px] h-[33.43px]">
+        PROJECT NAME
+      </b>
+    </Link>
+  );
+});
+
+export default CostLink;
