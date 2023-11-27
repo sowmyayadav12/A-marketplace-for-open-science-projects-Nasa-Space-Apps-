df41d03fc393f056e2102d271b156c7d0e632961
Golla Pujari Sowmya <121338921+sowmyayadav12@users.noreply.github.com>
2023-11-28 01:18:50 +0530

    Create g-e-t-p-r-o-j-e-c-t-s-a-t-y-o-u-r-d-o-o-r-s-t-e-p.js

---
+++ components/g-e-t-p-r-o-j-e-c-t-s-a-t-y-o-u-r-d-o-o-r-s-t-e-p.js
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+const GETPROJECTSATYOURDOORSTEP = memo(() => {
+  return (
+    <div className="absolute top-[704px] left-[calc(50%_-_169px)] w-[338px] h-[29px] text-left text-lgi text-black font-poppins">
+      <b className="absolute top-[0%] left-[0%]">
+        GET PROJECTS AT YOUR DOOR STEP
+      </b>
+    </div>
+  );
+});
+
+export default GETPROJECTSATYOURDOORSTEP;
