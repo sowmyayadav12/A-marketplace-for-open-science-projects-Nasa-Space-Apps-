39e683ba02c54e2e944068a44ce0437d3f68688e
Golla Pujari Sowmya <121338921+sowmyayadav12@users.noreply.github.com>
2023-12-02 00:18:25 +0530

    Create h-o-m-e.js

---
+++ Pages/pages/h-o-m-e.js
@@ -0,0 +1,141 @@
+import Link from "next/link";
+import GroupIcon from "../components/group-icon";
+import GroupComponent6 from "../components/group-component6";
+
+const HOME = () => {
+  return (
+    <div className="relative bg-white w-full h-[2589px] overflow-hidden text-left text-6xl text-white font-poppins">
+      <div className="absolute top-[0px] left-[0px] bg-white w-[1440px] h-[2589px] overflow-hidden">
+        <img
+          className="absolute h-full top-[0%] bottom-[0%] left-[calc(50%_-_720px)] max-h-full w-[1440px] object-cover"
+          alt=""
+          src="/image-26@2x.png"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[221px] left-[891px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/e-a-r-t-h-s-c-i-e-n-c-e"
+        />
+        <a className="[text-decoration:none] absolute top-[221px] left-[925px] rounded-11xl w-[355px] h-[333px] bg-[url('/rectangle19@2x.png')] bg-cover bg-no-repeat bg-[top] hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]" />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[568px] left-[989px] font-bold text-[inherit]"
+          href="/e-a-r-t-h-s-c-i-e-n-c-e"
+        >
+          EARTH SCIENCE
+        </Link>
+        <GroupIcon />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[774px] left-[891px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/s-p-a-c-e-s-c-i-e-n-c-e"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1147px] left-[989px] font-bold text-[inherit]"
+          href="/s-p-a-c-e-s-c-i-e-n-c-e"
+        >
+          SPACE SCIENCE
+        </Link>
+        <a className="[text-decoration:none] absolute top-[775px] left-[925px] rounded-11xl w-[355px] h-[333px] bg-[url('/rectangle21@2x.png')] bg-cover bg-no-repeat bg-[top] hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]" />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1327px] left-[908px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/m-a-t-e-r-i-a-l-s-c-i-e-n-c-e"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1700px] left-[1049px] font-bold text-[inherit] inline-block w-[142px] h-[22px]"
+          href="/m-a-t-e-r-i-a-l-s-c-i-e-n-c-e"
+        >
+          MATERIAL SCIENCE
+        </Link>
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1328px] left-[156px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/e-n-e-r-g-y-a-n-d-e-n-v"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1700px] left-[231px] font-bold text-[inherit]"
+          href="/e-n-e-r-g-y-a-n-d-e-n-v"
+        >
+          <p className="m-0">ENERY AND</p>
+          <p className="m-0"> ENVIRONMENTAL</p>
+        </Link>
+        <a className="[text-decoration:none] absolute top-[1327px] left-[179px] rounded-11xl w-[355px] h-[333px] bg-[url('/rectangle14@2x.png')] bg-cover bg-no-repeat bg-[top] hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]" />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1881px] left-[146px] rounded-11xl bg-gray-200 w-[389px] h-[463px]"
+          href="/o-t-h-e-r-s"
+        />
+        <img
+          className="absolute top-[1881px] left-[182px] rounded-11xl w-[355px] h-[333px] object-cover hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]"
+          alt=""
+          src="/rectangle-15@2x.png"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[2240px] left-[302px] font-bold text-[inherit]"
+          href="/o-t-h-e-r-s"
+        >
+          OTHERS
+        </Link>
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[775px] left-[156px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/l-i-f-e-s-c-i-e-n-c-e"
+        />
+        <a className="[text-decoration:none] absolute top-[775px] left-[179px] rounded-11xl w-[355px] h-[333px] bg-[url('/rectangle33@2x.png')] bg-cover bg-no-repeat bg-[top] hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]" />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[1147px] left-[271px] font-bold text-[inherit]"
+          href="/l-i-f-e-s-c-i-e-n-c-e"
+        >
+          LIFE SCIENCE
+        </Link>
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[221px] left-[156px] rounded-26xl bg-gray-200 w-[389px] h-[463px]"
+          href="/t-e-c-h-n-o-l-o-g-y"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[587px] left-[236px] font-bold text-[inherit]"
+          href="/t-e-c-h-n-o-l-o-g-y"
+        >
+          TECHNOLOGY
+        </Link>
+        <a className="[text-decoration:none] absolute top-[221px] left-[179px] rounded-11xl w-[355px] h-[333px] bg-[url('/rectangle35@2x.png')] bg-cover bg-no-repeat bg-[top] hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]" />
+        <img
+          className="absolute top-[1328px] left-[942px] rounded-11xl w-[355px] h-[333px] object-cover hover:animate-[1s_ease_0s_infinite_normal_none_scale-up] hover:opacity-[1]"
+          alt=""
+          src="/rectangle-27@2x.png"
+        />
+        <GroupComponent6
+          vector="/vector18.svg"
+          vector2="/vector19.svg"
+          yOUTop="2491px"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[2147px] left-[671px] rounded-41xl bg-darkolivegreen w-[561px] h-[67px]"
+          href="/g-r-o-u-p"
+        />
+        <img
+          className="absolute top-[2153px] left-[678px] rounded-[50%] w-[67px] h-14 object-cover"
+          alt=""
+          src="/ellipse-4@2x.png"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[2004px] left-[671px] rounded-41xl bg-darkslategray-100 w-[561px] h-[67px]"
+          href="/f-i-n-d1"
+        />
+        <img
+          className="absolute top-[2007px] left-[678px] rounded-[50%] w-[66px] h-[61px] object-cover"
+          alt=""
+          src="/ellipse-5@2x.png"
+        />
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[2020px] left-[778px] font-bold text-[inherit] inline-block w-[347px] h-[21px]"
+          href="/f-i-n-d1"
+        >
+          FIND SKILLED PEOPLE
+        </Link>
+        <Link
+          className="cursor-pointer [text-decoration:none] absolute top-[2157px] left-[782px] font-bold text-[inherit] inline-block w-[252px] h-6"
+          href="/g-r-o-u-p"
+        >
+          YOUR GROUPS
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HOME;
